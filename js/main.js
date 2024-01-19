e313f36695d91a5857cc82bcd4316b6edf824ead
eunah1202 <alsrldmsdk11@gmail.com>
2024-01-19 23:47:18 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -179,4 +179,24 @@ $(function(){
   });
 });
 
-
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 180,
+    stretch: 0,
+    depth: 500,
+    modifier: 1,
+    slideShadows:true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
